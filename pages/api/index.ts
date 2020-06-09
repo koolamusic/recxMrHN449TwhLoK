e8ec7bea32270482145ec1bcd69ea2177e618132
Andrew Miracle <me@andrewmiracle.com>
2020-06-09 20:07:53 +0000

    make api request async

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,30 +1,38 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { Client, Status } from '@googlemaps/google-maps-services-js';
+import { resolve } from 'dns';
 const client = new Client({});
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-	client
-		.placesNearby({
-			params: {
-				location: { lat: 5.6364025, lng: -0.1670703 },
-				radius: 10000,
-				type: 'hospital',
-				key: 'AIzaSyB01cSQiXTGE7IorUIw0nOQ_TbEXN5fpqU'
-			},
-			timeout: 1000 // milliseconds
-		})
-		.then(async (r) => {
-			if (r.data.status === Status.OK) {
-				await res.json(r.data.results);
-				res.statusCode = 200;
-			} else {
-				res.json(r.data.error_message);
-			}
-		})
-		.catch((e) => {
-			console.log(e);
-		});
+	return new Promise((resolve, reject) => {
+		client
+			.placesNearby({
+				params: {
+					location: { lat: 5.6364025, lng: -0.1670703 },
+					radius: 10000,
+					type: 'hospital',
+					key: 'AIzaSyB01cSQiXTGE7IorUIw0nOQ_TbEXN5fpqU'
+				},
+				timeout: 1000 // milliseconds
+			})
+			.then(async (r) => {
+				if (r.data.status === Status.OK) {
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'application/json');
+					res.setHeader('Cache-Control', 'max-age=180000');
+					await res.json(r.data.results);
+					resolve();
+				} else {
+					res.json(r.data.error_message);
+					resolve();
+				}
+			})
+			.catch((e) => {
+				console.log(e);
+				reject(e);
+			});
+	});
 };
 
 export default handler;
